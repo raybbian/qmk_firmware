e8ff21c52197fe7262ccd824fe5ee48bcbad9c7b
Raymond Bian <raybbian@gmail.com>
2024-11-12 12:30:14 -0500

    My config

--- keyboards/boardsource/lulu/keymaps/raybbian/keymap.c
+++ keyboards/boardsource/lulu/keymaps/raybbian/keymap.c
@@ -12,6 +12,8 @@ enum layers {
     _ADJUST
 };
 
+#include "combos.c"
+
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
@@ -19,33 +21,35 @@ enum layers {
 #define COLEMK DF(_COLEMAKDH)
 #define QWERTY DF(_QWERTY)
 
+#define CTRLESC LCTL_T(KC_ESC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAKDH] = LAYOUT(
         XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SLSH, KC_BSPC,
-        KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    QWERTY,  GAMING,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SCLN, RAISE,
-                                   KC_LALT, KC_LGUI, KC_ENT,  KC_LSFT, KC_SPC,  LOWER,   KC_UNDS, XXXXXXX
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    XXXXXXX, KC_BSPC,
+        CTRLESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+        RAISE,   KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    QWERTY,  GAMING,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SCLN, LOWER,
+                                   KC_LALT, KC_LGUI, KC_ENT,  KC_LSFT, KC_SPC,  KC_UNDS, KC_MINS, XXXXXXX
     ),
     [_QWERTY] = LAYOUT(
-        XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    COLEMK,  GAMING,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RAISE,
-                                   KC_LALT, KC_LGUI, KC_ENT,  KC_LSFT, KC_SPC,  LOWER,   KC_UNDS, XXXXXXX
+        _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+        _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
+        _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, _______,
+        _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    COLEMK,  _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+                                   _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_GAMING] = LAYOUT(
-        XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    COLEMK,  QWERTY,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RAISE,
-                                   KC_6,    KC_7,    KC_LALT, KC_SPC,  XXXXXXX, LOWER,   XXXXXXX, XXXXXXX
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_8,    COLEMK,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                   KC_6,    KC_7,    KC_LALT, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
     [_LOWER] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
         _______, KC_F11,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_PLUS, KC_ASTR, KC_AMPR, KC_F12,  _______,
         _______, XXXXXXX, KC_GRV,  KC_BSLS, KC_EQL,  KC_TILD,                   KC_EXLM, KC_MINS, KC_SLSH, KC_PIPE, XXXXXXX, _______,
-        _______, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, XXXXXXX, _______, _______, XXXXXXX, KC_RPRN, KC_RCBR, KC_RBRC, KC_RABK, _______,
+        _______, XXXXXXX, KC_LABK, KC_LBRC, KC_LPRN, KC_LCBR, _______, _______, KC_RCBR, KC_RPRN, KC_RBRC, KC_RABK, XXXXXXX, _______,
                                    _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_RAISE] = LAYOUT(
@@ -71,6 +75,11 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case KC_1 ... KC_0:
+        if (get_mods() == MOD_BIT(KC_LSFT))
+            return false;
+        break;
+    case KC_COMM:
+    case KC_DOT:
         if (get_mods() == MOD_BIT(KC_LSFT))
             return false;
         break;
