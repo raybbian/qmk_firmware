0ffc9c7daa770f9083f5cec5cc2a95e01d83f9e4
Raymond Bian <raybbian@gmail.com>
2024-12-20 12:20:22 -0500

    Updates to my config

--- keyboards/boardsource/lulu/keymaps/raybbian/combos.c
+++ keyboards/boardsource/lulu/keymaps/raybbian/combos.c
@@ -1,30 +1,34 @@
-const uint16_t PROGMEM at_combo[] = {KC_2, KC_W, COMBO_END};
+#include QMK_KEYBOARD_H
+#include "common.h"
+
+const uint16_t PROGMEM at_combo[]   = {KC_2, KC_W, COMBO_END};
 const uint16_t PROGMEM hash_combo[] = {KC_3, KC_F, COMBO_END};
-const uint16_t PROGMEM dlr_combo[] = {KC_4, KC_P, COMBO_END};
+const uint16_t PROGMEM dlr_combo[]  = {KC_4, KC_P, COMBO_END};
 const uint16_t PROGMEM perc_combo[] = {KC_5, KC_B, COMBO_END};
 const uint16_t PROGMEM circ_combo[] = {KC_6, KC_J, COMBO_END};
 const uint16_t PROGMEM plus_combo[] = {KC_7, KC_L, COMBO_END};
 const uint16_t PROGMEM astr_combo[] = {KC_8, KC_U, COMBO_END};
 const uint16_t PROGMEM ampr_combo[] = {KC_9, KC_Y, COMBO_END};
 
-const uint16_t PROGMEM grv_combo[] = {KC_W, KC_R, COMBO_END};
+const uint16_t PROGMEM lbrc_combo[] = {KC_W, KC_R, COMBO_END};
 const uint16_t PROGMEM bsls_combo[] = {KC_F, KC_S, COMBO_END};
-const uint16_t PROGMEM eql_combo[] = {KC_P, KC_T, COMBO_END};
+const uint16_t PROGMEM eql_combo[]  = {KC_P, KC_T, COMBO_END};
 const uint16_t PROGMEM tild_combo[] = {KC_B, KC_G, COMBO_END};
 const uint16_t PROGMEM exlm_combo[] = {KC_J, KC_M, COMBO_END};
 const uint16_t PROGMEM mins_combo[] = {KC_L, KC_N, COMBO_END};
 const uint16_t PROGMEM slsh_combo[] = {KC_U, KC_E, COMBO_END};
-const uint16_t PROGMEM pipe_combo[] = {KC_Y, KC_I, COMBO_END};
+const uint16_t PROGMEM rbrc_combo[] = {KC_Y, KC_I, COMBO_END};
 
-const uint16_t PROGMEM lbrc_combo[] = {KC_R, KC_X, COMBO_END};
+const uint16_t PROGMEM grv_combo[]  = {KC_R, KC_X, COMBO_END};
 const uint16_t PROGMEM lcbr_combo[] = {KC_S, KC_C, COMBO_END};
 const uint16_t PROGMEM lprn_combo[] = {KC_T, KC_D, COMBO_END};
 const uint16_t PROGMEM labk_combo[] = {KC_G, KC_V, COMBO_END};
 const uint16_t PROGMEM rabk_combo[] = {KC_M, KC_K, COMBO_END};
 const uint16_t PROGMEM rprn_combo[] = {KC_N, KC_H, COMBO_END};
 const uint16_t PROGMEM rcbr_combo[] = {KC_E, KC_COMM, COMBO_END};
-const uint16_t PROGMEM rbrc_combo[] = {KC_I, KC_DOT, COMBO_END};
+const uint16_t PROGMEM pipe_combo[] = {KC_I, KC_DOT, COMBO_END};
 
+// clang-format off
 combo_t key_combos[] = {
     COMBO(at_combo, KC_AT),
     COMBO(hash_combo, KC_HASH),
@@ -53,7 +57,8 @@ combo_t key_combos[] = {
     COMBO(rcbr_combo, KC_RCBR),
     COMBO(rbrc_combo, KC_RBRC),
 };
+// clang-format on
 
 bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode, keyrecord_t *record) {
-    return layer_state_is(_COLEMAKDH) || layer_state_is(_QWERTY);
+    return layer_state_is(_COLEMAKDH);
 }
--- keyboards/boardsource/lulu/keymaps/raybbian/keymap.c
+++ keyboards/boardsource/lulu/keymaps/raybbian/keymap.c
@@ -2,41 +2,24 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-
-enum layers {
-    _COLEMAKDH,
-    _QWERTY,
-    _GAMING,
-    _RAISE,
-    _LOWER,
-    _ADJUST
-};
-
+#include "common.h"
 #include "combos.c"
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
-
 #define GAMING DF(_GAMING)
 #define COLEMK DF(_COLEMAKDH)
-#define QWERTY DF(_QWERTY)
 
 #define CTRLESC LCTL_T(KC_ESC)
 
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAKDH] = LAYOUT(
         XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    XXXXXXX, KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SLSH, KC_BSPC,
         CTRLESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-        RAISE,   KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    QWERTY,  GAMING,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SCLN, LOWER,
+        RAISE,   KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    XXXXXXX, GAMING,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SCLN, LOWER,
                                    KC_LALT, KC_LGUI, KC_ENT,  KC_LSFT, KC_SPC,  KC_UNDS, KC_MINS, XXXXXXX
-    ),
-    [_QWERTY] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-        _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
-        _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, _______,
-        _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    COLEMK,  _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
-                                   _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_GAMING] = LAYOUT(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -48,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
         _______, KC_F11,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_PLUS, KC_ASTR, KC_AMPR, KC_F12,  _______,
-        _______, XXXXXXX, KC_GRV,  KC_BSLS, KC_EQL,  KC_TILD,                   KC_EXLM, KC_MINS, KC_SLSH, KC_PIPE, XXXXXXX, _______,
-        _______, XXXXXXX, KC_LABK, KC_LBRC, KC_LPRN, KC_LCBR, _______, _______, KC_RCBR, KC_RPRN, KC_RBRC, KC_RABK, XXXXXXX, _______,
+        _______, XXXXXXX, KC_LBRC, KC_BSLS, KC_EQL,  KC_TILD,                   KC_EXLM, KC_MINS, KC_SLSH, KC_RBRC, XXXXXXX, _______,
+        _______, XXXXXXX, KC_GRV,  KC_LCBR, KC_LPRN, KC_LABK, _______, _______, KC_RABK, KC_RPRN, KC_RCBR, KC_PIPE, XXXXXXX, _______,
                                    _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_RAISE] = LAYOUT(
@@ -67,22 +50,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    _______, _______, _______, _______, _______,  _______, _______, _______
     )
 };
+// clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-    case KC_1 ... KC_0:
-        if (get_mods() == MOD_BIT(KC_LSFT))
-            return false;
-        break;
-    case KC_COMM:
-    case KC_DOT:
-        if (get_mods() == MOD_BIT(KC_LSFT))
-            return false;
-        break;
-    }
     return true;
 }
