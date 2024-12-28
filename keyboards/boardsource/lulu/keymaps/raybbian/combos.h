b06e08662e6140bf09cd59ecd98b62bb6b8c01c2
Raymond Bian <raybbian@gmail.com>
2024-12-27 22:24:10 -0500

    upd

--- keyboards/boardsource/lulu/keymaps/raybbian/combos.h
+++ keyboards/boardsource/lulu/keymaps/raybbian/combos.h
@@ -11,22 +11,20 @@ const uint16_t PROGMEM astr_combo[] = {KC_8, KC_U, COMBO_END};
 const uint16_t PROGMEM ampr_combo[] = {KC_9, KC_Y, COMBO_END};
 
 const uint16_t PROGMEM lbrc_combo[] = {KC_W, KC_R, COMBO_END};
-const uint16_t PROGMEM bsls_combo[] = {KC_F, KC_S, COMBO_END};
-const uint16_t PROGMEM eql_combo[]  = {KC_P, KC_T, COMBO_END};
-const uint16_t PROGMEM tild_combo[] = {KC_B, KC_G, COMBO_END};
-const uint16_t PROGMEM exlm_combo[] = {KC_J, KC_M, COMBO_END};
-const uint16_t PROGMEM mins_combo[] = {KC_L, KC_N, COMBO_END};
-const uint16_t PROGMEM slsh_combo[] = {KC_U, KC_E, COMBO_END};
+const uint16_t PROGMEM lcbr_combo[] = {KC_F, KC_S, COMBO_END};
+const uint16_t PROGMEM lprn_combo[] = {KC_P, KC_T, COMBO_END};
+const uint16_t PROGMEM labk_combo[] = {KC_B, KC_G, COMBO_END};
+const uint16_t PROGMEM rabk_combo[] = {KC_J, KC_M, COMBO_END};
+const uint16_t PROGMEM rprn_combo[] = {KC_L, KC_N, COMBO_END};
+const uint16_t PROGMEM rcbr_combo[] = {KC_U, KC_E, COMBO_END};
 const uint16_t PROGMEM rbrc_combo[] = {KC_Y, KC_I, COMBO_END};
 
-const uint16_t PROGMEM grv_combo[]  = {KC_R, KC_X, COMBO_END};
-const uint16_t PROGMEM lcbr_combo[] = {KC_S, KC_C, COMBO_END};
-const uint16_t PROGMEM lprn_combo[] = {KC_T, KC_D, COMBO_END};
-const uint16_t PROGMEM labk_combo[] = {KC_G, KC_V, COMBO_END};
-const uint16_t PROGMEM rabk_combo[] = {KC_M, KC_K, COMBO_END};
-const uint16_t PROGMEM rprn_combo[] = {KC_N, KC_H, COMBO_END};
-const uint16_t PROGMEM rcbr_combo[] = {KC_E, KC_COMM, COMBO_END};
-const uint16_t PROGMEM pipe_combo[] = {KC_I, KC_DOT, COMBO_END};
+const uint16_t PROGMEM bsls_combo[] = {KC_S, KC_C, COMBO_END};
+const uint16_t PROGMEM eql_combo[]  = {KC_T, KC_D, COMBO_END};
+const uint16_t PROGMEM tild_combo[] = {KC_G, KC_V, COMBO_END};
+const uint16_t PROGMEM exlm_combo[] = {KC_M, KC_K, COMBO_END};
+const uint16_t PROGMEM mins_combo[] = {KC_N, KC_H, COMBO_END};
+const uint16_t PROGMEM pipe_combo[] = {KC_E, KC_COMM, COMBO_END};
 
 // clang-format off
 combo_t key_combos[] = {
@@ -39,15 +37,6 @@ combo_t key_combos[] = {
     COMBO(astr_combo, KC_ASTR),
     COMBO(ampr_combo, KC_AMPR),
 
-    COMBO(grv_combo, KC_GRV),
-    COMBO(bsls_combo, KC_BSLS),
-    COMBO(eql_combo, KC_EQL),
-    COMBO(tild_combo, KC_TILD),
-    COMBO(exlm_combo, KC_EXLM),
-    COMBO(mins_combo, KC_MINS),
-    COMBO(slsh_combo, KC_SLSH),
-    COMBO(pipe_combo, KC_PIPE),
-
     COMBO(lbrc_combo, KC_LBRC),
     COMBO(lcbr_combo, KC_LCBR),
     COMBO(lprn_combo, KC_LPRN),
@@ -56,9 +45,16 @@ combo_t key_combos[] = {
     COMBO(rprn_combo, KC_RPRN),
     COMBO(rcbr_combo, KC_RCBR),
     COMBO(rbrc_combo, KC_RBRC),
+
+    COMBO(bsls_combo, KC_BSLS),
+    COMBO(eql_combo, KC_EQL),
+    COMBO(tild_combo, KC_TILD),
+    COMBO(exlm_combo, KC_EXLM),
+    COMBO(mins_combo, KC_MINS),
+    COMBO(pipe_combo, KC_PIPE),
 };
 // clang-format on
 
 bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode, keyrecord_t *record) {
-    return layer_state_is(_COLEMAKDH);
+    return get_highest_layer(layer_state) == _COLEMAKDH;
 }
--- keyboards/boardsource/lulu/keymaps/raybbian/keymap.c
+++ keyboards/boardsource/lulu/keymaps/raybbian/keymap.c
@@ -3,7 +3,7 @@
 
 #include QMK_KEYBOARD_H
 #include "common.h"
-#include "combos.c"
+#include "combos.h"
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
@@ -15,7 +15,7 @@
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAKDH] = LAYOUT(
-        XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SLSH, KC_BSPC,
         CTRLESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
         RAISE,   KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    XXXXXXX, GAMING,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SCLN, LOWER,
