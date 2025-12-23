f200efb4afd7f51f30c068a781dbf82b1b9920be
Raymond Bian <raybbian@gmail.com>
2025-12-22 20:43:47 -0500

    upd

--- keyboards/boardsource/lulu/keymaps/raybbian/keymap.c
+++ keyboards/boardsource/lulu/keymaps/raybbian/keymap.c
@@ -22,17 +22,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    KC_LALT, KC_LGUI, KC_ENT,  KC_LSFT, KC_SPC,  KC_UNDS, KC_MINS, XXXXXXX
     ),
     [_GAMING] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_8,    COLEMK,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                   KC_6,    KC_7,    KC_LALT, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX, COLEMK,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LOWER,
+                                   KC_LALT, KC_LGUI, KC_ENT,  KC_SPC,  KC_GRV,  KC_UNDS, KC_MINS, XXXXXXX
     ),
     [_LOWER] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
         _______, KC_F11,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_PLUS, KC_ASTR, KC_AMPR, KC_F12,  _______,
-        _______, XXXXXXX, KC_LBRC, KC_BSLS, KC_EQL,  KC_TILD,                   KC_EXLM, KC_MINS, KC_SLSH, KC_RBRC, XXXXXXX, _______,
-        _______, XXXXXXX, KC_GRV,  KC_LCBR, KC_LPRN, KC_LABK, _______, _______, KC_RABK, KC_RPRN, KC_RCBR, KC_PIPE, XXXXXXX, _______,
+        _______, XXXXXXX, KC_LABK, KC_LCBR, KC_LPRN, KC_LBRC, _______, _______, KC_RBRC, KC_RPRN, KC_RCBR, KC_RABK, XXXXXXX, _______,
+        _______, XXXXXXX, KC_GRV,  KC_BSLS, KC_EQL,  KC_TILD,                   KC_EXLM, KC_MINS, KC_SLSH, KC_PIPE, XXXXXXX, _______,
                                    _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_RAISE] = LAYOUT(
