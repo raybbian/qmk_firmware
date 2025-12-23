5dd201c03f131208afbacea3dc14b27a4e88390a
Raymond Bian <raybbian@gmail.com>
2025-12-22 20:50:39 -0500

    oops

--- keyboards/boardsource/lulu/keymaps/raybbian/keymap.c
+++ keyboards/boardsource/lulu/keymaps/raybbian/keymap.c
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
         _______, KC_F11,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_PLUS, KC_ASTR, KC_AMPR, KC_F12,  _______,
-        _______, XXXXXXX, KC_LABK, KC_LCBR, KC_LPRN, KC_LBRC, _______, _______, KC_RBRC, KC_RPRN, KC_RCBR, KC_RABK, XXXXXXX, _______,
-        _______, XXXXXXX, KC_GRV,  KC_BSLS, KC_EQL,  KC_TILD,                   KC_EXLM, KC_MINS, KC_SLSH, KC_PIPE, XXXXXXX, _______,
+        _______, XXXXXXX, KC_LABK, KC_LCBR, KC_LPRN, KC_LBRC,                   KC_RBRC, KC_RPRN, KC_RCBR, KC_RABK, XXXXXXX, _______,
+        _______, XXXXXXX, KC_GRV,  KC_BSLS, KC_EQL,  KC_TILD, _______, _______, KC_EXLM, KC_MINS, KC_SLSH, KC_PIPE, XXXXXXX, _______,
                                    _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_RAISE] = LAYOUT(
